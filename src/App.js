c2dd4898cde0cb18fac1ce76a9e228f03e7f56a4
Saidali Saburov <ssaburov@velox.biz>
2021-06-30 12:50:46 +0500

    Homeworks #2.1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import './App.css';
 import React from 'react'
 import Post from "./components/Post/Post";
-import Message from "./components/Message/Message";
 
 function App() {
+    const post = {
+        id: 1,
+        author: {
+            id: 99,
+            avatar: 'https:lms.openjs.io/logo_js.svg',
+            name: 'OpenJs'
+        },
+        content: 'Наше крутое лого',
+        photo: 'https:lms.openjs.io/openjs.jpg',
+        hit: true,
+        likes: 1000,
+        likedByMe: true,
+        created: 1603501200
+    }
     return (
         <div className="App">
-            <Post content="Первый пост"/>
-            <Post content="Второй пост"/>
-            <Post content="Третий пост"/>
-            <Message text="Первое сообщение"/>
-            <Message text="Второе сообщение"/>
-            <Message text="Третье сообщение"/>
+            <Post post={post}/>
         </div>
     );
 }
--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import './Post.css'
 
-function Post(props) {
+function Post({post}) {
+    const {author} = post;
+
     return (
-        <div>
-            {props.content}
-        </div>
+        <article>
+            <header>
+                <img src={author.avatar} className="Post-avatar" width="50" height="50" alt={author.name}/>
+                <h5>{author.name}</h5>
+                <div>{post.created}</div>
+                {post.hit && <span>HIT</span>}
+            </header>
+            <div>
+                <div className="Post-content">{post.content}</div>
+                <img src={post.photo} alt='photo' className="Post-photo"/>
+            </div>
+            <footer>
+                <span className="Post-likes">
+                    <img src={post.likedByMe ? "https://lms.openjs.io/liked.svg" : "https://lms.openjs.io/unliked.svg"} width="20" height="20"/>
+                    <span className="Post-likes-count">{post.likes}</span>
+                </span>
+            </footer>
+        </article>
     );
 }
 
