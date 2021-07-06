8d6382237065cab973e2bb65eb3f5406ca4f97fa
Saidali Saburov <ssaburov@velox.biz>
2021-07-06 15:49:43 +0500

    Homeworks #3.1

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -2,15 +2,24 @@ import React from 'react';
 import './Post.css'
 import Tags from "../Tags/Tags";
 
-function Post({post}) {
+function Post({post, onLike, onRemove}) {
     const {author} = post;
     const {photo} = post;
+
+    const handleClick = (evt) => {
+        onLike(post.id)
+    }
+
+    const handleRemoveClick = (evt) => {
+        onRemove(post.id)
+    }
 
     return (
         <article>
             <header>
                 <img src={author.avatar} className="Post-avatar" width="50" height="50" alt={author.name}/>
                 <h5>{author.name}</h5>
+                <button onClick={handleRemoveClick}>удалить</button>
                 <div>{post.created}</div>
                 {post.hit && <span>HIT</span>}
             </header>
@@ -19,7 +28,7 @@ function Post({post}) {
                 {photo && <img src={photo.url} alt={photo.alt} className="Post-photo"/>}
             </div>
             <footer>
-                <span className="Post-likes">
+                <span className="Post-likes" onClick={handleClick}>
                     <img src={post.likedByMe ? "https://lms.openjs.io/liked.svg" : "https://lms.openjs.io/unliked.svg"}
                          width="20" height="20" alt='likes'/>
                     <span className="Post-likes-count">{post.likes}</span>
--- src/components/Wall/Wall.js
+++ src/components/Wall/Wall.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Post from "../Post/Post";
 
 function Wall() {
-    const posts = [
+    const [posts, setPosts] = useState([
         {
             id: 2,
             author: {
@@ -36,10 +36,25 @@ function Wall() {
             tags: ['deadline', 'homework'],
             created: 1603501200
         }
-    ]
+    ])
+
+    const handlePostLike = (id) => {
+        setPosts((prevState) => prevState.map(o => {
+            if (o.id !== id) {
+                return o;
+            }
+            const likedByMe = !o.likedByMe
+            const likes = likedByMe ? o.likes + 1 : o.likes - 1
+            return {...o, likedByMe, likes}
+        }))
+    }
+    const handlePostRemove = (id) => {
+        setPosts((prevState) => prevState.filter(o => o.id !== id))
+    }
+
     return (
         <div>
-            {posts.map(o => <Post key={o.id} post={o}/>)}
+            {posts.map(o => <Post key={o.id} post={o} onLike={handlePostLike} onRemove={handlePostRemove}/>)}
         </div>
     );
 }
